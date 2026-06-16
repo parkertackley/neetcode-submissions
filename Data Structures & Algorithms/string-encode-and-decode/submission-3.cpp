c09af6e34c7d9d4777763a2bd7098f206e9de370
Parker <parkertackley@gmail.com>
2026-06-16 16:58:41 -0400

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string res = "";
+        for (const string s : strs)
+        {
+            int length = s.size();
+            res += to_string(length) + '#' + s;
+        }
+        return res;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> res;
+
+        int i = 0;
+        while (i < s.size())
+        {
+            int j = i;
+            while (s[j] != '#')
+                j++;
+
+            int length = stoi(s.substr(i, j - i));
+            string word = s.substr(j + 1, length);
+            res.push_back(word);
+            i = j + 1 + length;
+
+        }
+        return res;
+    }
+};
