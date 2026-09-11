d84fd992bd0eca87dd6d469019fdee1474c9eed4
Parker <parkertackley@gmail.com>
2026-09-11 10:09:44 -0400

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        mp = {")": "(", "}": "{", "]": "["}
+
+        stack = []
+        for paren in s:
+            if paren in mp:
+                if stack and mp[paren] == stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(paren)
+
+        return True if not stack else False
